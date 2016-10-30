7e57576ebd034ba1a114bf7947ad0e8febf59775
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-10-30 20:35:11 +0000

    Transfer mongo delta from candidates service.

--- app/controllers/HealthController.scala
+++ app/controllers/HealthController.scala
@@ -2,17 +2,36 @@ package controllers
 
 import javax.inject._
 
-import play.api.libs.json._
+import db.MongoConnectivity
+import org.mongodb.scala.Document
+import org.mongodb.scala.bson.BsonString
+import play.api.Logger
+import play.api.libs.json.Json
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 @Singleton
-class HealthController @Inject() extends Controller {
+class HealthController @Inject()(mongo: MongoConnectivity) extends Controller {
+
   def alive = Action.async { request =>
-    Future {
-      Ok(Json.obj("status" -> "OK"))
+    mongo.appCollection.find().first().head.map { doc =>
+      extractAlive(doc).fold(NotFound(statusMessage("KO"))) { bs =>
+        val message = statusMessage(bs.getValue)
+        Logger.info(s"/alive 200 response: $message")
+        Ok(message)
+      }
+    }.recover {
+      case e =>
+        val message = errorMessage(e)
+        Logger.error(s"/alive 503 response $message")
+        ServiceUnavailable(message)
     }
   }
+
+  private def extractAlive(doc: Document) = doc.get[BsonString]("alive")
+
+  private def statusMessage(s: String) = Json.obj("status" -> s)
+
+  private def errorMessage(e: Throwable) = Json.obj("status" -> "KO", "error" -> e.getMessage)
 }
--- build.sbt
+++ build.sbt
@@ -1,15 +1,27 @@
+import com.typesafe.config.ConfigFactory
+
+enablePlugins(JavaServerAppPackaging)
+
+enablePlugins(DockerPlugin)
+
 name := """sdkman-hooks"""
 
-version := "1.0-SNAPSHOT"
+val conf = ConfigFactory.parseFile(new File("conf/application.conf")).resolve()
+
+version := conf.getString("application.version")
+
+packageName in Docker := "sdkman/sdkman-hooks"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.11.8"
 
+routesGenerator := InjectedRoutesGenerator
+
 libraryDependencies ++= Seq(
-  jdbc,
   cache,
   ws,
+  "org.mongodb.scala" %% "mongo-scala-driver" % "1.1.1",
   "org.scalatest" %% "scalatest" % "3.0.0" % Test,
   "info.cukes" %% "cucumber-scala" % "1.2.5" % Test,
   "info.cukes" % "cucumber-junit" % "1.2.5" % Test,
