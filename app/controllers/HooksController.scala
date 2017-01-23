e94c7b37763885ab5b1cc212bca27bf34b45ca73
Marco Vermeulen <vermeulen.mp@gmail.com>
2017-01-23 20:11:21 +0000

    Bring order to HooksController pattern match.

--- app/controllers/HooksController.scala
+++ app/controllers/HooksController.scala
@@ -18,24 +18,34 @@ class HooksController extends Controller {
       Logger.info(s"$phase install hook requested for: $candidate $version $platform")
 
       (phase, candidate, version, platform) match {
+
+        //POST: Mac OSX
         case (PostHook, Candidate.Java, "6u65", Platform.MacOSX) =>
           Ok(views.html.java_6u65_osx_post(candidate, version, Platform.MacOSX))
         case (PostHook, Candidate.Java, "7u79", Platform.MacOSX) =>
           Ok(views.html.java_7u79_osx_post(candidate, version, Platform.MacOSX))
-        case (PostHook, Candidate.Java, "8u111", Platform.Linux) =>
-          Ok(views.html.java_8u111_linux_post(candidate, version, Platform.Linux))
         case (PostHook, Candidate.Java, "8u111", Platform.MacOSX) =>
           Ok(views.html.java_8u111_osx_post(candidate, version, Platform.MacOSX))
         case (PostHook, Candidate.Java, "8u121", Platform.MacOSX) =>
           Ok(views.html.java_8u121_osx_post(candidate, version, Platform.MacOSX))
-        case (PostHook, Candidate.Java, "8u111", Platform.Windows64) =>
-          Ok(views.html.java_8u121_osx_post(candidate, version, Platform.MacOSX))
         case (PostHook, Candidate.Java, "9ea", Platform.MacOSX) =>
           Ok(views.html.java_9_ea_osx_post(candidate, version, Platform.MacOSX))
+
+        //POST: Linux
+        case (PostHook, Candidate.Java, "8u111", Platform.Linux) =>
+          Ok(views.html.java_8_linux_post(candidate, version, Platform.Linux))
+
+        //POST: Cygwin
+        case (PostHook, Candidate.Java, "8u111", Platform.Windows64) =>
+          Ok(views.html.java_8_cygwin_post(candidate, version, Platform.MacOSX))
+
+        //POST
         case (PostHook, Candidate.Java, _, _) =>
           NotFound
         case (PostHook, _, _, _) =>
           Ok(views.html.default_post(candidate, version, platform.name))
+
+        //PRE
         case (PreHook, Candidate.Java, _, _) =>
           Ok(views.html.java_pre(candidate, version))
         case (PreHook, _, _, _) =>
