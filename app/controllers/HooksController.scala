f303bb74230c7e7a007d14866cc955f49e68dde8
Marco Vermeulen <vermeulen.mp@gmail.com>
2017-05-28 11:23:26 +0100

    Bring back appropriate hooks for Java.

--- app/controllers/HooksController.scala
+++ app/controllers/HooksController.scala
@@ -14,20 +14,43 @@ class HooksController extends Controller {
 
   def hook(phase: String, candidateId: String, version: String, platformId: String) = Action.async { request =>
     Future {
-      implicit val candidate = Candidate(candidateId)
+      val candidate = Candidate(candidateId)
 
       val platform = Platform(platformId).getOrElse(Platform.Universal)
 
       Logger.info(s"$phase install hook requested for: $candidateId $version ${platform.name}")
 
       (phase, candidate, version, platform) match {
-        //POST
+
+        //POST: Mac OSX
+        case (PostHook, Candidate.Java, "6u65-apple", Platform.MacOSX) =>
+          Ok(views.txt.java_6u65_osx_post(candidate, version, Platform.MacOSX))
+        case (PostHook, Candidate.Java, _, Platform.MacOSX) =>
+          Ok(views.txt.default_post(candidate, version, Platform.MacOSX))
+
+        //POST: Linux
         case (PostHook, Candidate.Java, _, Platform.Linux) =>
           Ok(views.txt.java_linux_tarball_post(candidate, version, Platform.Linux))
+
+        //POST: Cygwin
+        case (PostHook, Candidate.Java, _, Platform.Windows64) =>
+          Ok(views.txt.default_post(candidate, version, Platform.Windows64))
+
+        //POST
+        case (PostHook, Candidate.Java, _, _) =>
+          NotFound
         case (PostHook, _, _, _) =>
           Ok(views.txt.default_post(candidate, version, platform))
 
         //PRE
+        case (PreHook, Candidate.Java, "4u19-oracle", Platform.Linux) =>
+          Ok(views.txt.java_pre_obcla(candidate, version))
+        case (PreHook, Candidate.Java, "5u22-oracle", Platform.Linux) =>
+          Ok(views.txt.java_pre_obcla(candidate, version))
+        case (PreHook, Candidate.Java, "5u22-oracle", Platform.Windows64) =>
+          Ok(views.txt.java_pre_obcla(candidate, version))
+        case (PreHook, Candidate.Java, "6u65-apple", Platform.MacOSX) =>
+          Ok(views.txt.java_pre_asla(candidate, version))
         case (PreHook, _, _, _) =>
           Ok(views.txt.default_pre(candidate, version, platform))
       }
--- features/install_hooks.feature
+++ features/install_hooks.feature
@@ -14,7 +14,19 @@ Feature: Install Hooks
 
 	Scenario: A platform specific Post Hook is served
 		Given a Hook is available for consumption
-		When I fetch a "post" hook for "java" "8u111" on "Linux"
+		When I fetch a "post" hook for "java" "8u111-zulu" on "Linux"
 		Then a 200 status code is received
-		And I receive a hook containing text: A Linux post-install hook was found for Java 8u111.
+		And I receive a hook containing text: A Linux post-install hook was found for Java 8u111-zulu.
 		And I receive a hook containing text: tar zxf "$binary_input" -C "$work_dir"
+
+	Scenario: A platform specific Pre Hook is served
+		Given a Hook is available for consumption
+		When I fetch a "pre" hook for "java" "6u65-apple" on "Darwin"
+		Then a 200 status code is received
+		And I receive a hook containing text: A pre-install hook was found for Java 6u65-apple.
+		And I receive a hook containing text: Apple requires that you agree with the Apple Software License Agreement
+
+	Scenario: A platform specific Post Hook is not found
+		Given no relevant Hook is available
+		When I fetch a "post" hook for "java" "8u111-oracle" on "FreeBSD"
+		Then a 404 status code is received
