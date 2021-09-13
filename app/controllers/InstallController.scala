623dbab818c996ba0eca8651466881add2a88d11
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-09-13 19:32:30 +0100

    Split install templates into beta and stable channels.

--- app/controllers/InstallController.scala
+++ app/controllers/InstallController.scala
@@ -1,10 +1,10 @@
 package controllers
 
-import javax.inject.Inject
 import play.api.Configuration
 import play.api.mvc.{AbstractController, ControllerComponents}
 import repo.ApplicationRepo
 
+import javax.inject.Inject
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class InstallController @Inject() (
@@ -13,14 +13,39 @@ class InstallController @Inject() (
     config: Configuration
 ) extends AbstractController(cc) {
 
-  lazy val fallbackVersion = config.get[String]("service.fallbackVersion")
+  private val stableBaseUrlO = configUrl("service.stableBaseUrl")
 
-  lazy val baseUrl = config.get[String]("service.baseUrl")
+  private val betaBaseUrlO = configUrl("service.betaBaseUrl")
 
-  def install(rcUpdate: Option[Boolean]) = Action.async { _ =>
+  def install(beta: Boolean, rcUpdate: Option[Boolean]) = Action.async { _ =>
     appRepo.findApplication().map { maybeApp =>
-      val version = maybeApp.map(_.stableCliVersion).getOrElse(fallbackVersion)
-      Ok(views.txt.install(version, baseUrl, rcUpdate.getOrElse(true)))
+      val response = for {
+        stableBaseUrl <- stableBaseUrlO
+        betaBaseUrl   <- betaBaseUrlO
+        app           <- maybeApp
+        stableVersion = app.stableCliVersion
+        betaVersion   = app.betaCliVersion
+      } yield
+        if (beta) {
+          Ok(
+            views.txt.install_beta(
+              cliVersion = betaVersion,
+              baseUrl = betaBaseUrl,
+              rcUpdate = rcUpdate.getOrElse(true)
+            )
+          )
+        } else {
+          Ok(
+            views.txt.install_stable(
+              cliVersion = stableVersion,
+              baseUrl = stableBaseUrl,
+              rcUpdate = rcUpdate.getOrElse(true)
+            )
+          )
+        }
+      response getOrElse ServiceUnavailable
     }
   }
+
+  private def configUrl(url: String): Option[String] = config.getOptional[String](url)
 }
