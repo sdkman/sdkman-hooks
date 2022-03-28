8b3dde3ccf4343ac2853a990421302aae9916da1
Marco Vermeulen <vermeulen.mp@gmail.com>
2022-03-28 10:08:14 +0100

    Align Platform inference with other projects.
    This is the first step to extracting `Platform` to the common domain library.
    This will also enable platform inference required for native binaries.

--- app/domain/domain.scala
+++ app/domain/domain.scala
@@ -11,35 +11,44 @@ object Candidate {
   val JMC    = Candidate("jmc")
 }
 
-case class Platform(identifier: String, name: String)
+case class Platform(distribution: String, name: String)
+
 object Platform {
 
-  def apply(id: String): Platform = id.toLowerCase match {
-    case LinuxPattern(_)         => Linux
-    case DarwinPattern(_)        => MacOSX
-    case WindowsCygwinPattern(_) => Windows64Cygwin
-    case WindowsMinGWPattern(_)  => Windows64MinGW
-    case "freebsd"               => FreeBSD
-    case "sunos"                 => SunOS
-    case _                       => Exotic
+  def apply(platformId: String): Platform = platformId.toLowerCase match {
+    // current
+    case "linuxx64"       => LinuxX64
+    case "linuxarm64"     => LinuxARM64
+    case "linuxarm32sf"   => LinuxARM32SF
+    case "linuxarm32hf"   => LinuxARM32HF
+    case "darwinx64"      => MacX64
+    case "darwinarm64"    => MacARM64
+    case CygwinPattern(c) => Windows64
+    // backward compat
+    case "darwin"         => MacX64
+    case "linux"          => LinuxX64
+    case "linux64"        => LinuxX64
+    case "freebsd"        => FreeBSD
+    case "sunos"          => SunOS
+    case _                => Exotic
   }
 
-  private val WindowsCygwinPattern = "(cygwin).*".r
+  private val CygwinPattern = "(cygwin|mingw64|msys).*".r
 
-  private val WindowsMinGWPattern = "(mingw64|msys).*".r
+  val LinuxX32     = Platform("LINUX_32", "Linux 32bit")
+  val LinuxX64     = Platform("LINUX_64", "Linux 64bit")
+  val LinuxARM32SF = Platform("LINUX_ARM32SF", "Linux ARM 32bit Soft Float")
+  val LinuxARM32HF = Platform("LINUX_ARM32HF", "Linux ARM 32bit Hard Float")
+  val LinuxARM64   = Platform("LINUX_ARM64", "Linux ARM 64bit")
+  val MacX64       = Platform("MAC_OSX", "macOS 64bit")
+  val MacARM64     = Platform("MAC_ARM64", "macOS ARM 64bit")
+  val Windows64    = Platform("WINDOWS_64", "Cygwin")
+  val FreeBSD      = Platform("FREE_BSD", "FreeBSD")
+  val SunOS        = Platform("SUN_OS", "Solaris")
 
-  private val LinuxPattern =
-    "(linux|linux64|linuxx64|linuxarm32|linuxarm64|linuxarm32sf|linuxarm32hf)".r
-
-  private val DarwinPattern = "(darwin|darwinx64|darwinarm64)".r
-
-  val Linux           = Platform("LINUX", "Linux")
-  val MacOSX          = Platform("MAC_OSX", "Mac OSX")
-  val Windows64Cygwin = Platform("CYGWIN", "Cygwin")
-  val Windows64MinGW  = Platform("MINGW", "MinGW")
-  val FreeBSD         = Platform("FREE_BSD", "FreeBSD")
-  val SunOS           = Platform("SUN_OS", "Solaris")
-  val Exotic          = Platform("UNIVERSAL", "Universal")
+  //fallback to UNIVERSAL for exotic (unsupported) platforms
+  //allows exotic platforms to install only UNIVERSAL candidates
+  val Exotic = Platform("UNIVERSAL", "Exotic")
 }
 
 object JdkDistro {
